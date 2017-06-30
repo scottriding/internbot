d39dc89ec56e1f774800652718e0ae15cdb67ad3
kathrynriding <kathryn@y2analytics.com>
2017-06-30 12:40:15 -0600

    Parser class create_survey method and Survey class init
    Re-ordered Parser class so the only method to be called by user is
    create_survey(). Survey class details to mirror each secondary branching
    in qsf file **before parsing.

--- internbot/base/qsf_parser.py
+++ internbot/base/qsf_parser.py
@@ -5,16 +5,27 @@ class Parser (object):
     def __init__ (self, path_to_qsf, path_to_output):
         self.path_to_qsf = path_to_qsf
         self.path_to_output = path_to_output
+    
+    def create_survey(self):
+        file = self.input_qsf()
+        description = file[u'SurveyEntry']
+        elements = file[u'SurveyElements']
+        block_details = []
+        question_details = []
+        for element in elements:
+            if 'BL' in element[u'Element']:
+                block_details = element
+            elif 'SQ' in element[u'Element']:
+                question_details = element
+            elif 'QC' in element[u'Element']:
+                question_count = element
+        survey = Survey(description[u'SurveyName'], block_details, question_details, question_count)
+        self.output_survey(survey)
         
     def input_qsf(self):
         with open(self.path_to_qsf) as file:
             survey_file = json.load(file)
-        self.create_survey(survey_file)    
-    
-    def create_survey(self, file):
-        description = file[u'SurveyEntry']
-        elements = file[u'SurveyElements']
-        survey = Survey(description[])
+        return survey_file
         
-    def output_survey(self):
-        pass
+    def output_survey(self, survey):
+        print survey
--- internbot/base/survey.py
+++ internbot/base/survey.py
@@ -1,16 +1,17 @@
 from block import Blocks
 
 class Survey(object):
-    def __init__(self, survey_name, survey_q_count, survey_bl_count):
+    def __init__(self, survey_name, survey_block_details, survey_question_details, survey_question_count):
         self.name = survey_name
-        self.q_count = survey_q_count
-        self.bl_count = survey_bl_count
-
+        self.block_details = survey_block_details
+        self.question_details = survey_question_details
+        self.question_count = survey_question_count
+        
     def add_block(self):
         pass
         
     def __repr__ (self):
         result = ""
-        result += "%s: %s/%s" % (self.name, str(self.blo_count), str(self.q_count))
+        result += "%s: %s" % (self.name, str(self.question_count))
         return result
         
