f1b3848026a62b05871a48480bb5af8cb2e5e4f5
kathrynriding <kathryn@y2analytics.com>
2017-06-30 14:22:41 -0600

    Fixed question_count parsing line

--- internbot/base/qsf_parser.py
+++ internbot/base/qsf_parser.py
@@ -8,20 +8,18 @@ class Parser (object):
     
     def create_survey(self):
         file = self.input_qsf()
-        description = file[u'SurveyEntry']
-        elements = file[u'SurveyElements']
-        block_details = []
-        question_details = []
+        name = file[u'SurveyEntry'][u'SurveyName']
+        elements = file[u'SurveyElements'] 
         for element in elements:
             if 'BL' in element[u'Element']:
                 block_details = element
             elif 'SQ' in element[u'Element']:
                 question_details = element
             elif 'QC' in element[u'Element']:
-                question_count = element
-        survey = Survey(description[u'SurveyName'], block_details, question_details, question_count)
+                question_count = element[u'SecondaryAttribute']
+        survey = Survey(name, block_details, question_details, question_count)
         self.output_survey(survey)
-        
+    
     def input_qsf(self):
         with open(self.path_to_qsf) as file:
             survey_file = json.load(file)
