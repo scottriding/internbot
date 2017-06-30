5c9e924a33aa98cddbb3fbdcf7d2125a6f7c4171
Scott Riding <scott@y2analytics.com>
2017-06-30 10:34:30 -0600

    Basic setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    name = 'internbot',
+    version = 0.00,
+    url = 'http://github.com/scottriding/internbot/',
+    authors=['Kathryn Riding', 'Scott Riding']
+)
