a6062678e8864423d9af4632c50050f8c33f8078
Scott Riding <scott@y2analytics.com>
2017-07-21 14:49:54 -0600

    A placeholder for an internbot CLI.

---
+++ internbot/internbot.py
@@ -0,0 +1,20 @@
+import argparse
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Automates Y2 Analytics reports.'
+    )
+
+import base
+import topline
+
+path_to_qsf = '/Users/y2analytics/Downloads/Weber_School_District_Potential_Bond_Viability_Survey.qsf'
+path_to_csv = '/Users/y2analytics/Downloads/Weber School District Weighted Freqs TMF v2.csv'
+path_to_template = '/Users/y2analytics/Dropbox (Y2 Analytics)/Y2 Analytics Team Folder/R&D/Topline automation/Template/topline_template.docx'
+path_to_output = '/Users/y2analytics/Desktop/Weber School District topline.docx'
+
+compile = base.QSFSurveyCompiler()
+survey = compile.compile(path_to_qsf)
+generator = topline.ReportGenerator(survey)
+generator.generate_report(path_to_csv, path_to_template, path_to_output)
