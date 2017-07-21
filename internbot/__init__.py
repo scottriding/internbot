e5d8a1cd5e5d534eadc610613550789abb4ba55e
Scott Riding <scott@y2analytics.com>
2017-07-21 14:20:59 -0600

    Clarify namespaces

--- internbot/__init__.py
+++ internbot/__init__.py
@@ -8,7 +8,7 @@ path_to_csv = '/Users/y2analytics/Downloads/Weber School District Weighted Freqs
 path_to_template = '/Users/y2analytics/Dropbox (Y2 Analytics)/Y2 Analytics Team Folder/R&D/Topline automation/Template/topline_template.docx'
 path_to_output = '/Users/y2analytics/Desktop/Weber School District topline.docx'
 
-compile = QSFSurveyCompiler()
+compile = base.QSFSurveyCompiler()
 survey = compile.compile(path_to_qsf)
-generator = ReportGenerator(survey)
+generator = topline.ReportGenerator(survey)
 generator.generate_report(path_to_csv, path_to_template, path_to_output)
