6312542410878d633ee64232e0a1fc40e78516c9
kathrynriding <kathryn@y2analytics.com>
2017-07-31 15:00:01 -0600

    Added composite question layout to powerpoint

--- internbot/topline/topline_ppt.py
+++ internbot/topline/topline_ppt.py
@@ -13,12 +13,22 @@ class ToplinePPT(object):
 
     def write_questions(self):
         for question in self.questions:
-            self.write_question(question)
+            if question.type == 'Composite':
+                self.write_composite_slide(question)
+            elif question.type == 'Meta':
+                pass
+            else:
+                self.write_basic_slide(question)
 
     def save_file(self, path_to_output):
         self.ppt.save(path_to_output)
 
-    def write_question(self, question):
+    def write_composite_slide(self, question):
+        slide = self.ppt.slides.add_slide(self.ppt.slide_layouts[3])
+        self.write_name(question.name, slide)
+        self.write_prompts(question, slide)
+
+    def write_basic_slide(self, question):
         slide = self.ppt.slides.add_slide(self.ppt.slide_layouts[1])
         self.write_name(question.name, slide)
         self.write_prompt(question.prompt, slide)
@@ -30,6 +40,14 @@ class ToplinePPT(object):
     def write_prompt(self, prompt, slide):
         body = slide.placeholders[1]
         body.text = prompt
+
+    def write_prompts(self, composite_question, slide):
+        overall_prompt = slide.placeholders[1]
+        overall_prompt.text = composite_question.prompt
+        sub_prompt = slide.placeholders[2]
+        sub_prompt.text = ''
+        for sub_question in composite_question.questions:
+            sub_prompt.text += sub_question.prompt + '\n'
 
     def get_ppt(self):
         return self.ppt
