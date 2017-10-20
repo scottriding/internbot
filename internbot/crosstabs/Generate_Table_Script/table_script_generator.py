9e45afba327f0f5234ab17aec4d2d62f963da17b
kathrynriding <kathryn@y2analytics.com>
2017-10-20 15:32:06 -0600

    Update to table_script_generator to fix sps syntax and add column definer code

--- internbot/crosstabs/Generate_Table_Script/table_script_generator.py
+++ internbot/crosstabs/Generate_Table_Script/table_script_generator.py
@@ -1,10 +1,13 @@
 import csv
 
 class TableScript(object):
+
+    def __init__(self):
+        self.grouped_question = []
     
     def compile_scripts(self, path_to_tables, path_to_output):
         table_path = str(path_to_tables) + '/Tables to run.csv'
-        output_path = str(path_to_output) + '/table script.txt'
+        output_path = str(path_to_output) + '/table script.sps'
         output = open(output_path, "w+")
         script = self.write_script(table_path, path_to_output)
         output.write(script)
@@ -13,6 +16,7 @@ class TableScript(object):
         result = ''
         with open(path_to_tables, 'rb') as table_file:
             column_specs = self.compile_specs(path_to_tables)
+            result += self.add_column_recode(column_specs)
             file = csv.DictReader(table_file, quotechar = '"')
             for table in file:
                 result += self.write_table(table, path_to_output)
@@ -37,27 +41,37 @@ class TableScript(object):
         return result
 
     def write_ctable(self, question, column_specs):
-        result = '* Custom Tables.\nCTABLES\n'
-        result += "  /FORMAT EMPTY=BLANK MISSING='.'\n  /SMISSING VARIABLE\n"
+        result = "CTABLES\n  /FORMAT EMPTY=BLANK MISSING='.'\n  /SMISSING VARIABLE\n"
         result += "  /VLABELS VARIABLES=%s " % question['VariableName']
         result += 'Total '
         for spec in column_specs:
-            result += 'C%s ' % spec
-        result += 'DISPLAY=DEFAULT\n'
-        result += "  /TABLE %s [C][COUNT F40.0, COLPCT.COUNT PCT40.0] BY Total [C] " % question['VariableName']
-        for spec in column_specs:
-            if '$' in spec:
-                temp = spec.replace("$", "")
-                result += '+ $C%s [C] ' %temp
+            if "$" in spec:
+                result += '$C%s ' % spec.replace("$","")
+                self.grouped_question.append(spec)
             else:
-                result+= '+ C%s [C] ' %spec
+                result += 'C%s ' % spec
+        result += 'DISPLAY=LABEL\n'
+        result += "  /TABLE %s [C] BY Total [C][COUNT F40.0, COLPCT.COUNT PCT40.0] " % question['VariableName']
+        for spec in column_specs:
+            if spec in self.grouped_question:
+                result += '+ $C%s [C] ' % spec.replace("$","")
+                result += '[COUNT F40.0, COLPCT.COUNT PCT40.0] '
+            else:
+                result += '+ C%s [C] ' % spec
+                result += '[COUNT F40.0, COLPCT.COUNT PCT40.0] '
         result += '\n  /SLABELS POSITION=ROW VISIBLE=NO\n'
         result += "  /CATEGORIES VARIABLES=%s " % question['VariableName']
-        result += 'ORDER=A KEY=VALUE EMPTY=INCLUDE TOTAL=YES POSITION=AFTER\n'
+        result += "ORDER=A KEY=VALUE EMPTY=INCLUDE TOTAL=YES LABEL='Sigma' POSITION=AFTER\n"
         result += '  /CATEGORIES VARIABLES=Total '
         for spec in column_specs:
-            result += '%s ' %spec
-        result += 'ORDER=A KEY=VALUE EMPTY=EXCLUDE\n'
+            if spec not in self.grouped_question:
+                result += 'C%s ' % spec
+        result += 'ORDER=A KEY=VALUE EMPTY=INCLUDE\n'
+        result += '  /CATEGORIES VARIABLES=Total '
+        for spec in column_specs:
+            if spec in self.grouped_question:
+                result += '$C%s ' % spec.replace("$","")
+        result += 'ORDER=A KEY=VALUE EMPTY=INCLUDE\n'
         result += '  /CRITERIA CILEVEL=95\n'
         result += '  /TITLES\n'
         result += "    TITLE='Table %s - %s: %s'\n" % (question['TableIndex'], question['VariableName'], question['Title'])
@@ -66,5 +80,19 @@ class TableScript(object):
         result += '  /COMPARETEST TYPE=PROP ALPHA=0.05 ADJUST=BONFERRONI ORIGIN=COLUMN INCLUDEMRSETS=YES\n'
         result += '    CATEGORIES=ALLVISIBLE MERGE=NO SHOWSIG=NO.\n\n'
         result += 'OMSEND.'
-        result += '\n\n\n'
+        return result
+
+    def add_column_recode(self, column_specs):
+        result = "* CODE FOR RECODING COLUMN VARIABLES IN DATA SCRUB SCRIPT.\n\n"
+        for spec in column_specs:
+            if "$" in spec:
+                column = spec.replace("$","")
+            else:
+                column = spec
+            result += "compute C%s = %s. \n" % (column, column)
+            result += "APPLY DICTIONARY \n  /FROM *\n  /SOURCE VARIABLES="
+            result += "%s\n  /TARGET VARIABLES=C%s\n" % (column, column)
+            result += "  /FILEINFO\n  /VARINFO LEVEL VALLABELS=REPLACE VARLABEL."
+            result += "\n  freq C%s %s.\n\n" % (column, column)
+        result += "\n"
         return result
