823addfeac1024a8450b029a9d9625e87f13748a
kathrynriding <kathryn@y2analytics.com>
2018-04-20 17:06:02 -0600

    Separate concerns in internbot organization

--- internbot/automate_MS.py
+++ internbot/automate_MS.py
@@ -1,4 +1,22 @@
+import argparse
 import base
+import sys
 
-compiler = base.QSFSurveyCompiler()
-print compiler.grab_scoring('/Users/y2analytics/Downloads/QSup_MSSA_SCA_Assessment_Development.qsf')
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Automates Scoring Microsoft Assessment Surveys'
+    )
+
+    parser.add_argument('qsf', help='path to the Qualtrics QSF file')
+    parser.add_argument('csv', help='path to csv with questions')
+    parser.add_argument('output', help='path to output folder')
+    
+    args = parser.parse_args()
+
+    if not args.qsf and not args.report and not args.output:
+        parser.print_help()
+        sys.exit()
+
+    compiler = base.QSFSurveyCompiler()
+    scores = compiler.grab_scoring(args.qsf)
